2e0bd127a36c766b680e8fb9cda08bc1a372aef1
joshchik665 <joshua.chik@ised-isde.gc.ca>
2024-10-29 16:04:52 -0400

    more reorganizing of the widgets and making

--- ui/fsw_gui/mode_spec.py
+++ ui/fsw_gui/mode_spec.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import (
     QVBoxLayout,
     QPushButton,
+    QLabel
 )
 from ui.common_gui.mode_super import ModeSuper
 from ui.common_gui.common_widgets import SpectralWidget
@@ -8,6 +9,18 @@ from ui.common_gui.common_widgets import SpectralWidget
 class ModeSpec(ModeSuper):
     def __init__(self, device, parent=None):
         super().__init__('Spectrum', device, parent)
+        
+        apply_button_layout = QVBoxLayout()
+        
+        heading = QLabel(f"Settings for mode: {self.mode}")
+        heading.setObjectName("heading")
+        apply_button_layout.addWidget(heading)
+        
+        self.apply_button = QPushButton("Apply All Settings")
+        self.apply_button.pressed.connect(self.apply)
+        apply_button_layout.addWidget(self.apply_button)
+        
+        self.content_layout.addLayout(apply_button_layout, 0, 0, 1, 2)
         
         self.setting_layout1 = QVBoxLayout()
         self.setting_layout2 = QVBoxLayout()
@@ -28,26 +41,28 @@ class ModeSpec(ModeSuper):
         self.create_place_setting_box_widget("Pre-Amp Value", self.setting_layout2)
         self.create_place_setting_box_widget("Pre-Amp Mode", self.setting_layout2)
         
-        self.apply_button = QPushButton("Apply All Settings")
-        self.apply_button.pressed.connect(self.apply)
-        self.setting_layout2.addWidget(self.apply_button)
-        
         self.setting_layout1.addStretch(1)
         self.setting_layout2.addStretch(1)
         
+        self.content_layout.addLayout(self.setting_layout1, 1, 0)
+        self.content_layout.addLayout(self.setting_layout2, 1, 1)
+        
+        
+        self.func_layout = QVBoxLayout()
+        
         self.abort_button = QPushButton("Abort")
+        self.abort_button.setFixedSize(150, 30)
         self.abort_button.pressed.connect(self.instrument.abort)
-        self.setting_layout2.addWidget(self.abort_button)
+        self.func_layout.addWidget(self.abort_button)
         
         self.sweep_button = QPushButton("Run Sweep")
+        self.sweep_button.setFixedSize(150, 30)
         self.sweep_button.pressed.connect(self.instrument.sweep)
-        self.setting_layout2.addWidget(self.sweep_button)
+        self.func_layout.addWidget(self.sweep_button)
         
-        self.setting_layout2.addStretch(1)
+        self.func_layout.addStretch(1)
         
-        self.content_layout.addLayout(self.setting_layout1, 0, 0)
-        self.content_layout.addLayout(self.setting_layout2, 0, 1)
-        
+        self.content_layout.addLayout(self.func_layout, 0, 3, 2, 1)
         
         self.graph_layout = QVBoxLayout()
         
@@ -56,7 +71,7 @@ class ModeSpec(ModeSuper):
         
         self.graph_layout.addStretch(1)
         
-        self.content_layout.addLayout(self.graph_layout, 0 ,2)
+        self.content_layout.addLayout(self.graph_layout, 0, 2, 2, 1)
 
 
 
