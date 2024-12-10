55889cdb0329168b78982f75920cffd2382a363b
joshchik665 <joshua.chik@ised-isde.gc.ca>
2024-12-10 13:24:02 -0500

    added comments

--- ui/common_gui/mode_default.py
+++ ui/common_gui/mode_default.py
@@ -1,7 +1,8 @@
+# mode_default.py
+
 from PySide6.QtWidgets import (
     QVBoxLayout,
-    QPushButton,
-    QLabel
+    QPushButton
 )
 from PySide6.QtCore import Qt
 
@@ -12,8 +13,10 @@ from device.setting_classes.mode_setting import ModeSetting
 
 class ModeDefault(ModeSuper):
     def __init__(self, device, parent=None):
+        """Default window layout if none is specified for this mode"""
         super().__init__('Default', device, parent)
         
+        # Creates all the Numerical Settings from the instrument's settings list
         layout1 = QVBoxLayout()
         for setting_name, setting_object in self.instrument.settings.items():
             if isinstance(setting_object, NumericalSetting):
@@ -21,6 +24,7 @@ class ModeDefault(ModeSuper):
         layout1.addStretch(1)
         self.content_layout.addLayout(layout1, 1, 0)
         
+        # Creates all the Mode Settings from the instrument's setting list
         layout2 = QVBoxLayout()
         for setting_name, setting_object in self.instrument.settings.items():
             if isinstance(setting_object, ModeSetting):
@@ -28,11 +32,13 @@ class ModeDefault(ModeSuper):
         layout2.addStretch(1)
         self.content_layout.addLayout(layout2, 1, 1)
         
+        # Adds a button to refresh all the values if a display settings is present
         if self.contains_class(self.instrument.settings, DisplaySetting):
             self.refresh_button = QPushButton("Refresh values")
             self.refresh_button.pressed.connect(self.verify_all_settings)
             self.content_layout.addWidget(self.refresh_button, 0, 2, alignment=Qt.AlignBottom)
         
+        # Adds all the display settings from the instrument's setting list
         layout3 = QVBoxLayout()
         for setting_name, setting_object in self.instrument.settings.items():
             if isinstance(setting_object, DisplaySetting):
@@ -42,6 +48,7 @@ class ModeDefault(ModeSuper):
     
     
     def contains_class(self, dictionary, cls):
+        """Dictionary contains at least one value of type cls"""
         for value in dictionary.values():
             if isinstance(value, cls):
                 return True
